476e15c62544080503425e380478ef01c124bdc9
soonwonh <77424795+soonwonh@users.noreply.github.com>
2022-02-12 16:00:13 +0900

    Update der.py

--- trainer/der.py
+++ trainer/der.py
@@ -1,1 +1,4 @@
-# Implementation of DER(Dynamically Expandable Representations)
+# Implementation of DER(Dynamically Expandable Representations) , CVPR 2021
+# https://arxiv.org/abs/2103.16788
+# https://github.com/Rhyssiyan/DER-ClassIL.pytorch
+
