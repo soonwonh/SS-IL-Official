e64039fbd3d1ada1f70d417f43e5b4924108b02e
soonwonh <77424795+soonwonh@users.noreply.github.com>
2022-02-12 23:54:53 +0900

    Update der.py

--- trainer/der.py
+++ trainer/der.py
@@ -2,3 +2,87 @@
 # https://arxiv.org/abs/2103.16788
 # https://github.com/Rhyssiyan/DER-ClassIL.pytorch
 
+from __future__ import print_function
+
+import copy
+import logging
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+
+import networks
+import trainer
+
+
+class Trainer(trainer.GenericTrainer):
+    def __init__(self, IncrementalLoader, model, args):
+        super().__init__(IncrementalLoader, model, args)
+        if 'Hinge' in self.args.date:
+            self.model.module.fc = NormedLinear(512, 1004).cuda()
+        self.loss = torch.nn.CrossEntropyLoss(reduction='mean')
+        
+    def balance_fine_tune(self):
+        self.update_frozen_model()
+        
+        self.incremental_loader.update_bft_buffer()
+        self.incremental_loader.mode = 'b-ft'
+        
+        for epoch in range(15):
+            self.train(epoch)
+        
+    def train(self, epoch):
+        
+        self.model.train()
+        print("Epochs %d"%epoch)
+        
+        tasknum = self.incremental_loader.t
+        end = self.incremental_loader.end
+        mid = self.seen_classes[-1]
+        
+        for data, target in tqdm(self.train_iterator):
+            data, target = data.cuda(), target.cuda()
+            
+            output = self.model(data)
+            
+            if tasknum > 0 and self.args.ss:
+                loss_CE_curr = 0
+                loss_CE_prev = 0
+                curr_mask = target >= mid
+                prev_mask = target < mid
+                curr_num = (curr_mask).sum().int()
+                prev_num = (prev_mask).sum().int()
+                batch_size = curr_num + prev_num
+                
+                loss_CE_curr = self.loss(output[curr_mask,mid:end], target[curr_mask]%(end-mid)) * curr_num
+                loss_CE_prev = 0
+                if prev_num > 0:
+                    loss_CE_prev = self.loss(output[prev_mask,:mid], target[prev_mask]) * prev_num
+                loss_CE = (loss_CE_curr + loss_CE_prev) / batch_size
+
+            else:
+                loss_CE = self.loss(output[:,:end], target)
+            
+            self.optimizer.zero_grad()
+            (loss_CE).backward()
+            self.optimizer.step()
+            
+    def weight_align(self):
+        end = self.train_data_iterator.dataset.end
+        start = end-self.args.step_size
+        weight = self.model.module.fc.weight.data
+        
+        prev = weight[:start, :]
+        new = weight[start:end, :]
+        print(prev.shape, new.shape)
+        mean_prev = torch.mean(torch.norm(prev, dim=1)).item()
+        mean_new = torch.mean(torch.norm(new, dim=1)).item()
+
+        gamma = mean_prev/mean_new
+        print(mean_prev, mean_new, gamma)
+        new = new * gamma
+        result = torch.cat((prev, new), dim=0)
+        weight[:end, :] = result
+        print(torch.mean(torch.norm(self.model.module.fc.weight.data[:start], dim=1)).item())
+        print(torch.mean(torch.norm(self.model.module.fc.weight.data[start:end], dim=1)).item())
